9761b649173647d1f39de05a7e81070d8a9aefdb
=Paivi <paivi.e.suomela@gmail.com>
2015-05-17 21:57:37 +0100

    updated media file storage

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -109,6 +109,13 @@ STATIC_ROOT = 'staticfiles'
 
 DEBUG = False
 
+AWS_QUERYSTRING_AUTH = False
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+MEDIA_URL = 'http://%s.s3.amazonaws.com/pictures/' % AWS_STORAGE_BUCKET_NAME
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+
 try:
     from .local_settings import *
 except ImportError:
